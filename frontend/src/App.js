1126530e9d662ec0a5614f296ff3e1c02d85f5ce
thngn22 <thangnguyen69962002@gmail.com>
2023-08-18 23:38:57 +0700

    frist-commot

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navigation from "./customer/components/Navigation/Navigation";
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      hello world
+    </div>
+  );
+}
+
+export default App;
