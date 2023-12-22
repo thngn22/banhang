817032b23670083694ea386384a517a865faea83
Vu Khang <vunguyentrungkhang@gmail.com>
2023-12-23 00:40:24 +0700

    config BE CORS

--- backend/src/main/java/com/ecomerce/roblnk/util/Side.java
+++ backend/src/main/java/com/ecomerce/roblnk/util/Side.java
@@ -3,7 +3,7 @@ package com.ecomerce.roblnk.util;
 import jakarta.servlet.http.HttpServletRequest;
 
 public class Side {
-    public static final String CLIENT_SITE_URL = "https://shoes-shop-vip.vercel.app/";
+    public static final String CLIENT_SITE_URL = "https://shoes-shop-vip.vercel.app";
     public static final String SERVER_SITE_URL = "https://shoes-shop-vip.up.railway.app";
     public static final String CLIENT_SITE_URL_ORDER_HISTORY = CLIENT_SITE_URL + "/history-order";
 
